75f2826bdb3e7b7b9f77ae875e0a1a4fba0535ba
AbbyANG <bena.ao310@gmail.com>
2023-12-05 11:36:12 -0500

    lesson 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
+
+
+    //*Method 1//
     public static void main(String[] args) {
         System.out.println("Chocolate vegan cheese cake");
+
+        // \n - newline
+        // \t - tab
+        // \b  - backspace
+        // The client said "more vegan cake please", then smiled.
+        System.out.println("The client sadig, 'more vegan cake please', then smiled");
+        System.out.println("The client sadig, \"more vegan cake please\", then smiled");
+        System.out.println("c:\\users\\bill");
+
+        Vanilla();     //Telling the vanilla method when to run, 'transfer control' to Vanilla method, so it can run // method call//
+        // can call this as many times, and placed on any line in main
+
     }
+
+   //*Method 2//
+    public static void Vanilla() {
+        System.out.println("berries");
+        System.out.println("banana");
+        System.out.println("avocado");//when the body of Vanilla method is reached, main gains control again,
+        // and since Vanilla method in the main is the last the program ends
+
+    }
+
+    //few notes - println is a method in the system, it is set up like the main and vanilla methods I've created
+    //          - Java is like a recipe, ingredients and the recipe(method(s))
+
+
+
 }

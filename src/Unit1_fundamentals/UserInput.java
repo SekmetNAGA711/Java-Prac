5a6741e5aa25892d893d53fa2128886eb8142b54
AbbyANG <bena.ao310@gmail.com>
2023-12-26 20:30:00 -0500

    updated userinput

--- src/Unit1_fundamentals/UserInput.java
+++ src/Unit1_fundamentals/UserInput.java
@@ -17,7 +17,7 @@ public class UserInput {
         System.out.println("what is your name?");
 
         String myName = scanner.next();
-        System.out.println(myName + " is a name nice");
+        System.out.println(myName + " is a nice name");
 
 
 
@@ -26,6 +26,22 @@ public class UserInput {
          int myAge = scanner.nextInt();
 
         System.out.println(myAge + " is perfect");
+        System.out.println("Your name is " + myName + " and you are " + myAge + " years old.");
+
+
+        System.out.println("How tall are you in meters?");
+        double myHeight = scanner.nextDouble();
+        System.out.println("you are " + myHeight + " meters tall");
+
+
+        System.out.format("I have %.2f cats, %d parrots, and 1 %s ", 7.9, 2, "panther");// format specifier 'd' is for integers, 's' for strings %.2 is round to 2 decimal place 'f' for decimal/float
+
+        double averageMaleHeight = myHeight -1.80;
+        double averageFemaleHeight = myHeight - 1.73;
+
+        System.out.println("\n The difference between you height and the average is: ");
+        System.out.format("%.2fm if you are a male ", averageMaleHeight);
+        System.out.format("%.2fm if you are a female ", averageFemaleHeight);
 
 
 

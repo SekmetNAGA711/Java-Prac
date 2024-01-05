b3638c4351a2dc4fa4d5a1c1721ad56e50d18dfb
AbbyANG <bena.ao310@gmail.com>
2024-01-05 11:06:16 -0500

    if else statement

---
+++ src/Unit1_fundamentals/IfEsleStatements.java
@@ -0,0 +1,51 @@
+package Unit1_fundamentals;
+
+import java.util.Scanner;
+
+public class IfEsleStatements {
+
+    public static void main(String[] args)
+
+    {
+        Scanner scanner = new Scanner(System.in);
+
+     if (10>5) {
+//         if the condition is true the program will run the body, if it is false the program will ignore the body completely
+         System.out.println("this is true");
+     }
+
+        System.out.println("Welcome to this super fast rollercoaster!");
+        System.out.print("please enter you height in centimeters:");
+
+        int height = scanner.nextInt();
+
+
+        if(height < 130) {
+
+            System.out.println("Sorry you are below the height requirement");
+        }
+        else if (height > 210) {
+            System.out.println("sorry you are above the height requirement");
+        }
+        else {
+
+            System.out.println("Get ready for a great ride!");
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}

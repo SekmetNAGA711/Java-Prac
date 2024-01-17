b533fc119d96347c721b13008cdfe2b80e41d1a6
AbbyANG <bena.ao310@gmail.com>
2024-01-17 10:21:27 -0500

    updated

--- src/Unit1_fundamentals/IfEsleStatements.java
+++ src/Unit1_fundamentals/IfEsleStatements.java
@@ -10,7 +10,7 @@ public class IfEsleStatements {
         Scanner scanner = new Scanner(System.in);
 
      if (10>5) {
-//         if the condition is true the program will run the body, if it is false the program will ignore the body completely
+//       if the condition is true the program will run the body, if it is false the program will ignore the body completely
          System.out.println("this is true");
      }
 
@@ -20,9 +20,9 @@ public class IfEsleStatements {
         int height = scanner.nextInt();
 
 
-        if(height < 140) {
+        if(height <= 140) {
 
-            System.out.println("Sorry you are below the height requirement");
+            System.out.println("Get ready for a great ride, hope you enjoy it!");
         }
         else if (height < 210 || height == 210) {
             System.out.println("sorry you are above the height requirement");
@@ -30,14 +30,37 @@ public class IfEsleStatements {
 
         else {
 
-            System.out.println("Get ready for a great ride, hope you enjoy it!");
+            System.out.println("Sorry, you are no can do");
         }
+
+        compareStrings ();
+
 
     }
 
+    public static void compareStrings () {
+
+        System.out.println("pasta".equals("pasta")); //comparing strings
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Age: ");
+
+        int age = scanner.nextInt();
+
+        System.out.println("Do you have a VIP Pass? yes/no: ");
+
+        String vipPassReply = scanner.next();
 
 
+        if(age >= 18 && vipPassReply.equals("yes") || age >= 70) {
+            System.out.println("thanks, go on in");
+        }
+        else{
+            System.out.println("sorry, you can not pass.");
+        }
 
+    }
 
 
 

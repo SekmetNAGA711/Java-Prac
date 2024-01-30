43615d15766d365fd15e685d75bc2cc39df553bb
AbbyANG <bena.ao310@gmail.com>
2024-01-30 10:52:28 -0500

    updated

--- src/Unit1_fundamentals/LexicalScope.java
+++ src/Unit1_fundamentals/LexicalScope.java
@@ -16,10 +16,12 @@ public class LexicalScope {
 
 
 
-        int numberOfPets; //numberOfPets would need to be declared outside the if statement
+        int numberOfPets = -1; //numberOfPets would need to be declared outside the if statement and set to 0; sometimes it is better
+        //to give an invalid value -1 because if the user enters 0 we would not know if the value is from the system set or the user
 
         if(true) {// Note: if this is false the code within the brackets will not run; which will result in numberOfPets in
-            // System.out.println("you have " + numberOfPets) to give an error, because no int variable was initialized
+            // System.out.println("you have " + numberOfPets) to give an error, because no int variable was initialized, this is
+           // why numberOfPets is given a value of 0;
 
 
             Scanner scanner = new Scanner(System.in);
@@ -28,7 +30,7 @@ public class LexicalScope {
 //           ****** int numberOfPets = scanner.nextInt(); // numberOfPets can only be used inside the if statement giving an error when we
 //           to use it anywhere else*******
         }
-           System.out.println("you have " + numberOfPets);// this would be an error if the numberOfPets is declared inside
+           System.out.println("you have " + numberOfPets + " pets");// this would be an error if the numberOfPets is declared inside
 //          the if statement;***********
 
 
---
+++ src/Unit1_fundamentals/SwitchStatement.java
@@ -0,0 +1,26 @@
+package Unit1_fundamentals;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+    public static void main(String[] args)
+    {
+        //the switch statement is useful for comparing one variable with a number of values
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if(num == 1) {
+            System.out.println("You entered one");
+        }
+        else if(num ==2){
+            System.out.println("You entered 2");
+        }
+
+    }
+
+
+
+
+}

f2b5be69043908e0ff296709e7ccf5a7e517d263
AbbyANG <bena.ao310@gmail.com>
2024-02-21 11:05:19 -0500

    updated

--- src/Unit1_fundamentals/Loops.java
+++ src/Unit1_fundamentals/Loops.java
@@ -13,13 +13,24 @@ public class Loops {
         }
 
 
+        System.out.println();
+
+
+        for(int i = 1250; i >= 480; i--) {
+
+            System.out.println(i);
+        }
+
+
+        System.out.println();
+
 
         int i = 0;  //creating a variables ensures that the condition will be false at some point
 
         while(i < 10) {//The difference between a while loop and an if statement is that if a condition is met the body of program is executed and ends,
             // In the while statement when the end of the body is reached the program goes back up to the top and check the condition again, if it true it will
             //run the body again, it will keep repeating this until the condition is false, it will also skip the body of the while statement and move on.
-            System.out.println("Test");
+            System.out.println(i);
             i++; // same as i = i+1 or i =+ 1
 
         }

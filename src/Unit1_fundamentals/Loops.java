fbe3f3624782ecbf8c96e91c863440422cd48b4c
AbbyANG <bena.ao310@gmail.com>
2024-04-17 08:54:48 -0400

    updated

--- src/Unit1_fundamentals/Loops.java
+++ src/Unit1_fundamentals/Loops.java
@@ -29,7 +29,7 @@ public class Loops {
 
         int i = 0;  //creating a variables ensures that the condition will be false at some point
 
-        while(i < 18) {//The difference btw a while loop and an if statement is that if a condition is met the body of program is executed and ends,
+        while(i < 17) {//The difference btw a while loop and an if statement is that if a condition is met the body of program is executed and ends,
             // In the while statement when the end of the body is reached the program goes back up to the top and check the condition again, if it true it will
             //run the body again, it will keep repeating this until the condition is false, it will also skip the body of the while statement and move on.
             System.out.println(i);

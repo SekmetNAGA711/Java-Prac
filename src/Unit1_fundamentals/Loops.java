8bdc0130cda74ccfd9b258114c1b1000ab4289d2
AbbyANG <bena.ao310@gmail.com>
2024-04-24 11:15:34 -0400

    updated

--- src/Unit1_fundamentals/Loops.java
+++ src/Unit1_fundamentals/Loops.java
@@ -60,7 +60,7 @@ public class Loops {
 
         System.out.println();
 
-        for(int i = 1; i<=7; i++) {
+        for(int i = 1; i<=8; i++) {
             if(i==4) {
                 continue;
             }
